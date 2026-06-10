78107de6281d41bccfc40f4372e146e00913865d
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-06-09 21:05:23 -0300

    Add: Implements addColor with strings containing hexadecimal formated color

--- src/palette.h
+++ src/palette.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <fstream>
+#include <string>
 #include <vector>
 
 struct Color {
@@ -21,4 +23,25 @@ public:
   }
 
   void addColor(const Color &color) { m_colors.push_back(color); }
+
+  void addColor(std::string_view color_hex) {
+    Color color{};
+    for (size_t i{0}; i < color_hex.size() - 1; i += 2) {
+      if (color_hex[i] == '#')
+        i++;
+
+      std::string color_digit{color_hex[i], color_hex[i + 1]};
+
+      if (i == 0 or i == 1)
+        color.r = static_cast<std::uint8_t>(std::stoi(color_digit, 0, 16));
+
+      if (i == 2 or i == 3)
+        color.g = static_cast<std::uint8_t>(std::stoi(color_digit, 0, 16));
+
+      if (i == 4 or i == 5)
+        color.b = static_cast<std::uint8_t>(std::stoi(color_digit, 0, 16));
+    }
+
+    m_colors.push_back(color);
+  }
 };
