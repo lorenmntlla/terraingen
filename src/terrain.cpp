003f9e1aaf03d89df482ba8e209cba217598ba1d
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-06-17 10:24:09 -0300

    feat(terrain): implement constructor and destructor

---
+++ src/terrain.cpp
@@ -0,0 +1,17 @@
+#include "../include/terrain.h"
+#include <stdexcept>
+
+Terrain::Terrain(dimension_t side)
+    : m_side{(side > 1 and side % 2 == 1) ? side : 0}, m_rugosity{},
+      m_terrain{(m_side > 1 and m_side % 2 == 1)
+                    ? new altitude_t[m_side * m_side]()
+                    : nullptr} {
+  if (m_side == 0)
+    throw std::invalid_argument(
+        "Terrain must have an odd side greater than 1");
+}
+
+Terrain::~Terrain() {
+  delete[] m_terrain;
+  m_terrain = nullptr;
+}
