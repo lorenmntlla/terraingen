470ee72e558624834a15cd69597860e0ef434644
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-06-24 00:34:34 -0300

    feat(terrain): defines new members for proper diamond-square implementation

--- include/terrain.h
+++ include/terrain.h
@@ -1,16 +1,25 @@
 #pragma once
 #include "dimension.h"
+#include <random>
 #include <string>
 
 typedef int8_t altitude_t;
 
 class Terrain {
-  dimension_t m_side;
-  float m_rugosity;
+  const dimension_t m_side;
+
+  double m_rugosity;
+  altitude_t m_range;
+
+  std::mt19937 m_generator;
 
   altitude_t *m_heightmap;
 
-  bool isPowOfTwo(dimension_t a);
+  bool isPowOfTwo(dimension_t a) const;
+
+  altitude_t random();
+  void diamond(dimension_t boundary, dimension_t dx = 0, dimension_t dy = 0);
+  void square(dimension_t boundary, dimension_t dx = 0, dimension_t dy = 0);
 
 public:
   Terrain(dimension_t side = 3);
@@ -25,9 +34,11 @@ public:
 
   bool saveFile(const std::string &fileName) const;
 
-  bool generate(dimension_t side, float rugosity);
+  bool generate(double rugosity);
 
   altitude_t &operator()(dimension_t x, dimension_t y);
+
+  altitude_t *data();
 
   bool readFile(const std::string &fileName);
 
