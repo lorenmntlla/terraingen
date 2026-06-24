fc4b1b5912636419ff12e832637e7def7bdb0953
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-06-24 13:59:06 -0300

    chore: integrate image object in driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include "../include/terrain.h"
+#include "color.h"
 #include "dimension.h"
+#include "image.h"
 #include <iostream>
 
 int main(int argc, char **argv) {
+  if (argc < 3) {
+    std::cout << "Usage: " << "terraingen" << " side rugosity\n";
+    return 1;
+  }
+
   dimension_t side = (dimension_t)atoi(argv[1]);
 
   Terrain terreno{side};
+  Image red{side, side};
+  Image green{side, side};
+  Image blue{side, side};
 
   terreno.generate(atof(argv[2]));
 
   for (dimension_t height{0}; height < side * side; height++) {
-    if (height % (side) == 0 and height != 0)
-      std::cout << '\n';
+    channel_t c{(channel_t)terreno.data()[height]};
 
-    std::cout << (int)terreno.data()[height] << '\t';
+    red.data()[height] = {c, 0, 0};
+    green.data()[height] = {0, c, 0};
+    blue.data()[height] = {0, 0, c};
   }
 
-  std::cout << '\n';
+  red.savePPM("terrain_red.ppm");
+  green.savePPM("terrain_green.ppm");
+  blue.savePPM("terrain_blue.ppm");
+
   return 0;
 }
