f696078888a8490868c045bcb1bdd561cf020df6
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-07-06 22:00:44 -0300

    feat(color): scalar product

--- include/color.h
+++ include/color.h
@@ -7,4 +7,8 @@ struct Color {
   channel_t r;
   channel_t g;
   channel_t b;
+
+  template <typename T> Color operator*(T a) {
+    return {channel_t(r * a), channel_t(g * a), channel_t(b * a)};
+  }
 };
